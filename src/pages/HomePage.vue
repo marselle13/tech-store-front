83aed2920378da7228ce859ae095650cfa9beff9
marselle13 <nika.gogua8@gmail.com>
2023-08-10 17:20:56 +0400

    style: main slider

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,3 +1,46 @@
 <template>
-  <h1>Home</h1>
+  <div>
+    <swiper navigation class="main max-w-[90rem] mx-auto relative" :slides-per-view="1">
+      <swiper-slide>
+        <img :src="SliderImage" alt="image" class="w-full h-[20rem]" />
+      </swiper-slide>
+      <swiper-slide>
+        <img :src="SliderImage2" alt="image" class="w-full h-[20rem]" />
+      </swiper-slide>
+    </swiper>
+  </div>
 </template>
+<script setup>
+import SliderImage from '@/assets/msi-slide.png'
+import SliderImage2 from '@/assets/msi-slide-2.png'
+import { Swiper, SwiperSlide } from 'swiper/vue'
+import { Navigation } from 'swiper/modules'
+import SwiperCore from 'swiper'
+
+SwiperCore.use([Navigation])
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+</script>
+<style>
+.main .swiper-button-next,
+.main .swiper-button-prev {
+  width: 46px;
+  height: 79px;
+  background-repeat: no-repeat;
+}
+.main .swiper-button-prev {
+  background-image: url('../assets/back.svg');
+  left: 0;
+}
+
+.main .swiper-button-next {
+  background-image: url('../assets/next.svg');
+  right: 0;
+}
+
+.main .swiper-button-next::after,
+.main .swiper-button-prev::after {
+  content: '';
+}
+</style>
